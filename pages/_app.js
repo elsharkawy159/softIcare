053415f2e14662e84f61aa349b0b59a3b5e79ba0
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-07-25 14:09:26 +0300

    e

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import "mdb-ui-kit/css/mdb.min.css";
+import Head from "next/head.js";
+import Script from "next/script.js";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script //MDB
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"
+      />
+    </>
+  );
 }
